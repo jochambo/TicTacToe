59f961105998ea992176a8447f91408604e232b3
Joey Chamberlin <joey.chamberlin@gmail.com>
2014-10-15 16:03:41 -0500

    Added specs for making a move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,10 @@
 require 'rspec'
-
 require_relative '../game'
-require_relative '../ai'
 
 describe "Game" do
-  describe "new game" do
-    let(:game) { Game.new  }
+  let(:game) { Game.new  }
+
+  context "new game" do
 
     it "should have an empty board" do
       expect(game.empty?).to be_truthy
@@ -23,15 +22,31 @@ describe "Game" do
     it "should not have a winner" do
       expect(game.winner).to be nil
     end
+
+    it "should not be over" do
+      expect(game.over?).to be_falsey
+    end
+  end
+
+  context "make a move" do
+    it "should allow for a move in an open cell" do
+      expect(game.make_move(4)).to be_truthy
+    end
+    it "should not allow for a move to an occupied cell" do
+      game.make_move(4)
+      expect(game.make_move(4)).to be_falsey
+      expect(game.board[4]).to eq 'X'
+    end
+    it "should alternate between players" do
+      game.make_move(4)
+      game.make_move(5)
+      expect(game.board[4]).to eq 'X'
+      expect(game.board[5]).to eq 'O'
+    end
+    it "should indicate if the center spot is taken" do
+      expect(game.center_taken?).to be_falsey
+      game.make_move(4)
+      expect(game.center_taken?).to be_truthy
+    end
   end
 end
-
-# describe "AI" do
-#   describe 'next turn' do
-#     let(:game){ Game.new }
-#     let(:computer){ AI.new }
-#     it "should choose corner if available" do
-#       expect(game.best_move).to eq 0 || 2 || 6 || 8
-#     end
-#   end
-# end
