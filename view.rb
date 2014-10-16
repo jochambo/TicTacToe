bf2f3c6b622c6f4b927cd9a1c6eb66cc2e0b0cc2
Joey Chamberlin <joey.chamberlin@gmail.com>
2014-10-15 20:06:51 -0500

    Updated status messages, refactored initialization

--- view.rb
+++ view.rb
@@ -1,20 +1,40 @@
 class View
+
   BOARD = " X | X | X \n-----------\n X | X | X \n-----------\n X | X | X "
+  STATUS = {
+    start: "Shall we play a game?",
+    draw: "It's a draw... \nThe only winning move is not to play.\nPress 'R' to reset.",
+    win: " wins!\nThe only winning move is not to play.\nPress 'R' to reset.",
+    bad_move: "Not a valid move. Try again."
+  }
+
+  attr_reader :position, :status
 
   def initialize(game)
     @game = game
     @position = 0
-    @board = BOARD
   end
 
   def draw_screen
     [draw_board, status_line, position_line, instructions].join("\n")
   end
 
+  def view_status
+
+  end
+
   def move(x,y)
     x = (@position + x) % 3
     y = ((@position / 3) + y) % 3
     @position = x + (y * 3)
+  end
+
+  def attempt_position
+    if @game.valid_position?(@position)
+      set_position
+    else
+      @status = STATUS[:bad_move]
+    end
   end
 
   def set
@@ -25,7 +45,7 @@ class View
 
   def draw_board
     index = -1
-    @board.gsub(" X ") do
+    BOARD.gsub(" X ") do
       index += 1
       field = @game.board[index] ? @game.board[index] : " "
       @position == index ? "(#{field})" : " #{field} "
@@ -42,15 +62,11 @@ class View
 
   def status_line
     if @game.winner
-      "'#{@game.winner}' wins! Press 'R' to reset."
+      "'#{@game.winner}'" + STATUS[:win]
     elsif @game.over?
-      "It's a draw... Press 'R' to reset."
+      STATUS[:draw]
     else
       "Let's play!"
     end
   end
 end
-
-# game = Game.new
-# view = View.new(game)
-# p view.draw_board
